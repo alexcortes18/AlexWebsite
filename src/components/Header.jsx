cbed8cd8addfd1a5d14fbd558633dda9dfdf2171
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-03 14:54:36 +0800

    first batch of starting files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <div className="header">
+            <h1>{"</>Python + React"}</h1>
+            <ul className="nav-links"> 
+                <a href="/">Home</a>
+                <a href="/">CV</a>
+                <a href="/">Portfolio</a>
+            </ul>
+        </div>
+    );
+}
