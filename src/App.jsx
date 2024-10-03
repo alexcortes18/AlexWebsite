6e6cbe9ea38d8672b7653b2fe24e0fcc9c5ac757
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-03 17:13:08 +0800

    first page done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <Header />
       <MainContent></MainContent>
+      <MainContent></MainContent>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
-export default function Header(){
+export default function Header() {
     return (
-        <div className="header">
-            <h1>{"</>Python + React"}</h1>
-            <ul className="nav-links"> 
-                <a href="/">Home</a>
-                <a href="/">CV</a>
-                <a href="/">Portfolio</a>
-            </ul>
-        </div>
+        <>
+            <div className="header">
+                <h1>{"</>Python + React"}</h1>
+                <div>
+                    <ul className="nav-links">
+                        <a href="/">Home</a>
+                        <a href="/">CV</a>
+                        <a href="/">Portfolio</a>
+                        {/* <div>
+                            <div className="small-lines"></div>
+                            <div className="small-lines"></div>
+                            <div className="small-lines"></div>
+                        </div> */}
+                    </ul>
+
+                    <hr className="nav-divider" />
+                </div>
+
+            </div >
+        </>
     );
 }
