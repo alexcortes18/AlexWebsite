16070c95c9103e895c09776ff2d455a164ef47da
Alejandro Cortes <alex.cortes.i10@gmail.com>
2024-10-10 22:58:14 +0800

    final touches of basic Modal

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -15,7 +15,31 @@ export default function Modal({ open, onClose }) {
     return (
         <dialog ref={dialog} onClose={onClose} className="modal">
             <h2>Contact me at:</h2>
-            <button onClick={onClose}>Close</button>
+            <div className="modal-content">
+                <p>
+                    <strong><label>Email:</label></strong>
+                    <br></br>
+                    <text>alex.cortes.i10@gmail.com</text>
+                </p>
+                <p>
+                    <strong><label>Phone Number:</label></strong>
+                    <br></br>
+                    <text>+886 0958309287</text>
+                </p>
+                <p>
+                    <strong><label>Line ID:</label></strong>
+                    <br></br>
+                    <text>alexjcortes</text>
+                </p>
+                <p>
+                    <strong><label>LinkedIn:</label></strong>
+                    <br></br>
+                    <a href="https://www.linkedin.com/in/alejandro-izaguirre-225b42157/">Alex's Linkedin</a>
+                </p>
+            </div>
+            <div className="modal-controls">
+                <button onClick={onClose}>Close</button>
+            </div>
         </dialog>
     );
 }
